379f41dbb8e836e3a56a19407b2a4f6f3e0927dc
Denis <ceasar174@gmail.com>
2023-01-26 20:33:38 +0500

    End run program

--- Program.cs
+++ Program.cs
@@ -2,26 +2,27 @@
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-string [] array = new string[n]; //создаем массив
+string[] array = new string[n]; //создаем массив
 for (int i = 0; i < array.Length; i++) //наполянем массив данными
 {
     Console.Write($"Введите {i + 1} элемент массива: ");
     array[i] = Console.ReadLine();
 }
-string [] newarray = new string[array.Length];
-Console.WriteLine($"[{string.Join(", ", array)}]"); //вывод начального массива для проверки
-
-// for (int i = 0; i < array.Length; i++)
-// {
-// if (string [i] )
+string[] newarray = new string[array.Length];
+// Console.WriteLine($"[{string.Join(", ", array)}]"); //вывод начального массива для проверки
 
 
-// }
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    string number = Convert.ToString(array[i]);
+    int count = number.Length;
+    // Console.Write($"Длинна элемента"{count}");
+    if (count <= 3)
+    {
+        newarray[j] = array[i];
+        j++;
+    }
+}
+Console.WriteLine($"[{string.Join(", ", newarray)}]");
 
-// foreach (int element in numbers)
-
-// string number = Convert.ToString(Console.ReadLine());
-// int count = number.Length;
-
-
-Console.WriteLine($"[{string.Join(", ", array)}]");
