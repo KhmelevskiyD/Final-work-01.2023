d09735a72f1d58065ced0fec186b89b4b5392d55
Denis <ceasar174@gmail.com>
2023-01-26 20:04:23 +0500

    add kod start array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] array = new string[n]; //создаем массив
+for (int i = 0; i < array.Length; i++) //наполянем массив данными
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    array[i] = Console.ReadLine();
+}
+string [] newarray = new string[array.Length];
+Console.WriteLine($"[{string.Join(", ", array)}]"); //вывод начального массива для проверки
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (string [i] )
+
+
+// }
+
+// foreach (int element in numbers)
+
+// string number = Convert.ToString(Console.ReadLine());
+// int count = number.Length;
+
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
